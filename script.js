70de9d8af0159fb9acca5625fe2bd93130060886
jaysinhzala <jaysinhzala1@gmail.com>
2025-06-18 15:42:59 -0400

    Used DOM to select HTML elements

---
+++ script.js
@@ -0,0 +1,6 @@
+const locationInput = document.querySelector('.ocationInput');
+const searchBtn = document.querySelector('.searchButton');
+const wheatherInfo = document.querySelector('.weather-info');
+const location = document.querySelector('.location');
+const temp = document.querySelector('.temperature');
+const description = document.querySelector('.description');
